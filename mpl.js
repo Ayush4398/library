7f2f09577789a2768627494fb8398445bcd9af8b
Ayushpicturesque <agarwalayush1998@gmail.com>
2021-07-05 16:50:33 +0530

    Add files via upload

---
+++ mpl.js
@@ -0,0 +1,6 @@
+$(".quotes").click(function(){
+  $(".picquotefront").removeClass("disp").css({"transform": "rotateY(30deg)", "color": "#FF616"});;
+});
+$(".quoteswithout").click(function(){
+  $(".picquotefront").addClass("disp").css({"transform": "rotateY(30deg)", "color": "#FF616"});;
+});
